f643c5e602c76fb2cc4f13540c66a31470155523
Austin Lin <marcus.gallagher@gmail.com>
2014-03-17 23:31:55 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bandwagon::Application.config.secret_key_base = '46772cb8a67748ccb380fc197a5a5e5174f75264fa680081fe8bf5b6ba358cf19c629680c95b0331402ff8eb529ee3494db9195a2cee1ff376d551cd17347290'
