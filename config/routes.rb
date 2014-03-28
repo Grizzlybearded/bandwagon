c35e912ca718ab661581069cd929e66faddc91ca
Austin Lin <marcus.gallagher@gmail.com>
2014-03-27 22:31:37 -0400

    added eblast route and put eblast in header link

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Bandwagon::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/about"
   get "static_pages/test"
+  get "static_pages/eblast"
 
   root to: 'static_pages#home'
 
