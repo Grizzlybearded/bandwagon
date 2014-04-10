6a7baa13566a2b03e48fc4c0f4a673f999cfc4cc
Austin Lin <marcus.gallagher@gmail.com>
2014-04-10 00:59:01 -0400

    adding new image

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,7 +10,7 @@ class StaticPagesController < ApplicationController
   end
 
   def test
-  	list = ["marcus.gallagher@gmail.com", "martin.kleinbard@gmail.com"]
+  	list = ["marcus.gallagher@gmail.com"]
   	category = Category.where(show_to_users: true).first
   	list.each do |person|
   		NewsletterMailer.biweekly(category, person).deliver
