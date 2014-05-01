360d107134ffef54e8f3b9675c254ce11e39aa20
Austin Lin <marcus.gallagher@gmail.com>
2014-05-01 11:01:05 -0400

    testing mobile font size

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,7 +11,7 @@ class StaticPagesController < ApplicationController
   end
 
   def test
-  	list = ["andrea.kleinbard@gmail.com"]
+  	list = ["marcus.gallagher@gmail.com","martin.kleinbard@gmail.com"]
   	category = Category.where(show_to_users: true).first
   	list.each do |person|
   		NewsletterMailer.biweekly(category, person).deliver
